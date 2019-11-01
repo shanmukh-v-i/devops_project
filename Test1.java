044daa8cdcdffed764270a6b3ff5423210a1df45
shanmukh-v-i <35656192+shanmukh-v-i@users.noreply.github.com>
2019-11-01 23:32:21 +0530

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,13 @@
+package com.amit.javaTest;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		int g=3;
+		System.out.println(++g *8);
+		System.out.println(g++ *8);
+		System.out.println(++g);
+
+	}
+
+}
